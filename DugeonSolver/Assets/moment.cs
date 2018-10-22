fa4fa6915325f8e7c374a26e0723d13a0fbebb1f
Kamox <kmail.pijarowski@gmail.com>
2018-10-22 12:32:42 +0200

    przyciski

---
+++ DugeonSolver/Assets/moment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moment : MonoBehaviour {
+
+    float DirectionX;
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(DirectionX* 10, 0);
+	}
+}
