79df0f6cdc27ec46c80acc1eefdaae2ce30a342b
Kamox <kmail.pijarowski@gmail.com>
2018-10-23 11:05:44 +0200

    guzik

---
+++ DugeonSolver/Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+    
+    [SerializeField]
+    private float speed;
+    private Button nagore;
+    private Vector2 kierunek;
+    BoxCollider2D player;
+    
+    // Use this for initialization
+    void Start () {
+        nagore.onClick.AddListener(Move);
+	}
+
+    private void Move()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Move(player);
+	}
+    public void Move(BoxCollider2D player)
+    {
+        transform.Translate(kierunek * speed * Time.deltaTime);
+    }
+}
--- DugeonSolver/Assets/w_górę.cs
+++ DugeonSolver/Assets/w_górę.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class w_górę : MonoBehaviour {
+    public float moveSpeed;
+    private bool moveRight;
+    private Vector3 maxSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        if (moveRight == true)
+        { 
+            GetComponent<Rigidbody2D>().AddForce(transform.right );
+        }
+        else if (moveRight == false)
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.right );
+        }
+    }
+
+    public void MoveLeft()
+    {
+        transform.position -= transform.forward * Time.deltaTime * moveSpeed;
+
+        // moveRight = false;
+    }
+
+
+    public void MoveRight()
+    {
+        transform.position += transform.forward * Time.deltaTime * moveSpeed;
+    }
 }
+ // moveRight = true;
